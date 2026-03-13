91ba8c5c7e309663fe888198a0ffc343048437e5
GauravMandli <155796426+GauravMandli@users.noreply.github.com>
2026-03-13 23:57:37 +0530

    Update settings.py

--- placement_portal/settings.py
+++ placement_portal/settings.py
@@ -59,13 +59,14 @@ AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 add this
 ]
 
 ROOT_URLCONF = 'placement_portal.urls'
